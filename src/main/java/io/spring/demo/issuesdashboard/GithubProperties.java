716f214e7ca85b03d007d1e617db2f99e72777c5
Rustam Karimov <rkarimov.r@gmail.com>
2018-11-13 23:57:17 +0400

    Fetch data from github

---
+++ src/main/java/io/spring/demo/issuesdashboard/GithubProperties.java
@@ -0,0 +1,17 @@
+package io.spring.demo.issuesdashboard;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("github")
+public class GithubProperties {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
